f7f2495e225b698ab70ff0cfc314c790ab7dd82e
slinger0225 <694600580@qq.com>
2019-04-22 18:03:40 +0800

    adapt to server configuration

--- huacai_server/manager.py
+++ huacai_server/manager.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from application import app,manager
 from flask_script import Server
--- huacai_server/web/controllers/index.py
+++ huacai_server/web/controllers/index.py
@@ -3,7 +3,7 @@ from flask import Blueprint,request, json,jsonify
 
 route_index = Blueprint( 'index_page',__name__ )
 
-@route_index.route("/",methods=['POST'])
+@route_index.route("/")
 def index():
     #data = json.loads(request.form.get('data'))
 
@@ -12,8 +12,8 @@ def index():
     #lesson = data["lesson"]
     #score = data["score"]
 
-    info = dict()
-    info['name'] = "pengshuang"
-    info['lesson'] = "lesson"
-    info['score'] = "score"
-    return jsonify(info)
+    #info = dict()
+    #info['name'] = "pengshuang"
+    #info['lesson'] = "lesson"
+    #info['score'] = "score"
+    return "欢迎来到花菜音乐小程序"
