6227ed9fd9fa01fb055ce5e1719b9d0993a1d8e5
slinger0225 <694600580@qq.com>
2019-04-24 13:05:55 +0800

    initialize for test environment

--- huacai_server/web/controllers/post_data.py
+++ huacai_server/web/controllers/post_data.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint,request
 from common.libs.sent_all import sent_rec
+from common.libs.sent_all_test import sent_rec as sent_rec_test
 import json
 
 route_post = Blueprint('post', __name__)
@@ -14,5 +15,16 @@ def index():
     userInputStory = json.loads(userInputStory)
     rec_result = {}
     rec_result = sent_rec(userInputStory)
+    print(rec_result)
+    return json.dumps(rec_result, ensure_ascii=False)
+
+@route_post.route("/test", methods=['POST'])
+def index():
+    #userID = request.values.get("userID")
+    #userID = json.loads(userID)
+    userInputStory = request.values.get("userInputStory")
+    userInputStory = json.loads(userInputStory)
+    rec_result = {}
+    rec_result = sent_rec_test(userInputStory)
     print(rec_result)
     return json.dumps(rec_result, ensure_ascii=False)
